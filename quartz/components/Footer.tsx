13bb807f405b25cfa529ed6ddfd126eb5b650d50
jeff-liang <46398134+jeff-liang@users.noreply.github.com>
2026-03-22 14:37:34 -0500

    Update Footer.tsx

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,17 +13,16 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
+	<p>
+	Created with joy, good faith, and yes, ❤️, by Jeffrey Liang © {year}
+	</p>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
         </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
+        <p>
+	You can reach me on Twitter: <a href="https://x.com/Arethys">@Arethys</a>
+	</p>
       </footer>
     )
   }
