4f18051c777dc390cebfb4b2ac91d97b4713af86
jeff-liang <46398134+jeff-liang@users.noreply.github.com>
2026-04-13 09:14:40 -0500

    Make epistemic status visible~

---
+++ quartz/components/EpistemicStatus.tsx
@@ -0,0 +1,19 @@
+import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/epistemicStatus.scss"
+
+export default (() => {
+  function EpistemicStatus({ fileData }: QuartzComponentProps) {
+    const status = fileData.frontmatter?.epistemic_status
+    if (!status) return null
+
+    return (
+      <div class="epistemic-status">
+        <span class="epistemic-label">epistemic status</span>
+        <span class="epistemic-value">{status as string}</span>
+      </div>
+    )
+  }
+
+  EpistemicStatus.css = style
+  return EpistemicStatus
+}) satisfies QuartzComponentConstructor
